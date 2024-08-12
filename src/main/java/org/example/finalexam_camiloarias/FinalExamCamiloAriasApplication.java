2aaeaf09994c8b28f667d0816d533085c420397c
Camilo Arias <132008606+Cami2905@users.noreply.github.com>
2024-08-12 16:19:07 -0700

    First commit

---
+++ src/main/java/org/example/finalexam_camiloarias/FinalExamCamiloAriasApplication.java
@@ -0,0 +1,13 @@
+package org.example.finalexam_camiloarias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalExamCamiloAriasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalExamCamiloAriasApplication.class, args);
+	}
+
+}
